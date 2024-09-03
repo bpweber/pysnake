c03f2d52825300aec2b05b57762dc2c8f510ab26
Bryce Weber <bpweber98@gmail.com>
2024-09-02 18:02:39 -0700

    changes to input handling

--- pysnake.py
+++ pysnake.py
@@ -2,15 +2,26 @@ import pygame
 import time
 import random
 
-UP = 0
-DOWN = 1
-LEFT = 2
-RIGHT = 3
+UP = 1
+DOWN = -1
+LEFT = -2
+RIGHT = 2
+
+key_maps = {
+    pygame.K_UP: UP,
+    pygame.K_w: UP,
+    pygame.K_DOWN: DOWN,
+    pygame.K_s: DOWN,
+    pygame.K_LEFT: LEFT,
+    pygame.K_a: LEFT,
+    pygame.K_RIGHT: RIGHT,
+    pygame.K_d: RIGHT
+}
 
 move_speed = 15
 
-win_x = 720
-win_y = 480
+win_x = 1280
+win_y = 720
 
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
@@ -20,7 +31,7 @@ blue = pygame.Color(0, 0, 255)
 
 pygame.init()
 
-pygame.display.set_caption('Random Text')
+pygame.display.set_caption('pysnake')
 game_window = pygame.display.set_mode((win_x, win_y))
 
 fps = pygame.time.Clock()
@@ -44,13 +55,13 @@ score = 0
 
 def display_score(choice, color, font, size):
     score_font = pygame.font.SysFont(font, size)
-    score_surface = score_font.render(f'Score: {score}', True, color)
+    score_surface = score_font.render(f'SCORE {score}', True, color)
     score_rect = score_surface.get_rect()
     game_window.blit(score_surface, score_rect)
 
 def game_over():
     font = pygame.font.SysFont('JetBrainsMono Nerd Font', 50)
-    game_over_surface = font.render(f'Your score was: {score}', True, red)
+    game_over_surface = font.render(f'GAME OVER | SCORE {score}', True, red)
     game_over_rect = game_over_surface.get_rect()
     game_over_rect.midtop = (win_x/2, win_y/2)
     game_window.blit(game_over_surface, game_over_rect)
@@ -62,24 +73,10 @@ def game_over():
 if __name__ == '__main__':
     while True:
         for e in pygame.event.get():
-            if e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_UP:
-                    new_dir = UP
-                if e.key == pygame.K_DOWN:
-                    new_dir = DOWN
-                if e.key == pygame.K_LEFT:
-                    new_dir = LEFT
-                if e.key == pygame.K_RIGHT:
-                    new_dir = RIGHT
+            if e.type == pygame.KEYDOWN and e.key in key_maps:
+                new_dir = key_maps[e.key]
 
-        if new_dir == UP and dir != DOWN:
-            dir = UP
-        if new_dir == DOWN and dir != UP:
-            dir = DOWN
-        if new_dir == LEFT and dir != RIGHT:
-            dir = LEFT
-        if new_dir == RIGHT and dir != LEFT:
-            dir = RIGHT
+        dir = dir if dir + new_dir == 0 else new_dir
 
         if dir == UP:
             snake_pos[1] -= 10
@@ -93,6 +90,7 @@ if __name__ == '__main__':
         snake_bod.insert(0, list(snake_pos))
         if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
             score += 10
+            move_speed += 2
             food_spawn = False
         else:
             snake_bod.pop()
@@ -107,7 +105,7 @@ if __name__ == '__main__':
         for pos in snake_bod:
             pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
 
-        pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+        pygame.draw.rect(game_window, red, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
 
         if snake_pos[0] < 0 or snake_pos[0] > win_x-10:
             game_over()
@@ -118,7 +116,8 @@ if __name__ == '__main__':
             if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
                 game_over()
 
-        display_score(1, white, 'JetBrainsMono Nerd Font', 20)
+        display_score(1, white, 'JetBrainsMono Nerd Font', 40)
 
         pygame.display.update()
         fps.tick(move_speed)
+
